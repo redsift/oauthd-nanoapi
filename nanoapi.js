1c3aff754f71dd40dbd04220b279b5e8053949f9
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2016-07-01 15:19:02 +0100

    write slack app with scopes and everything

--- nanoapi.js
+++ nanoapi.js
@@ -16,23 +16,39 @@ module.exports = function (env) {
         //request.params.client_id;
         //request.params.client_secret;
         var user = { id: 'admin' };
-        var body = { name: request.params.guid, domains: ['https://sso.redsift.io', 'http://localhost:8081'] };
+        var body = { name: request.params.guid, domains: ['https://sso.redsift.io', 'http://localhost:8081'], key: request.params.guid, secret: request.params.guid };
         env.data.apps.create(body, user, function (err, result) {
           if (err) {
             // Send error
+            console.error(err);
             return;
           }
           env.events.emit('app.create', user, result);
           console.log('key=', result.key);
           var providerBody = {
             parameters: {
-              client_id: request.params.client_id,
-              client_secret: request.params.client_secret
+              client_id: request.params.slack.client_id,
+              client_secret: request.params.slack.client_secret,
+              scope: 'bot identify chat:write:bot channels:read users:read'
             }
           };
-          env.data.apps.addKeyset(result.key, 'slack', providerBody, function (err, result1) {
-            console.log('addKeySet', err, result1);
+          env.data.apps.addKeyset(result.key, 'slack', providerBody, function (err1) {
+            console.log('addKeySet', err1);
+            if (err1) {
+              // Send error
+              console.error(err1);
+              return;
+            }
+
             // Send response result.key
+            env.data.apps.setBackend(result.key, 'node', {}, function (err2) {
+              if (err2) {
+                // Send error
+                console.error(err2);
+                return;
+              }
+
+            });
           });
         });
       }
