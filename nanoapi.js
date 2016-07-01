3657aaaf340b225b4a6a57d571006e1e48e77492
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2016-07-01 16:09:19 +0100

    added co

--- nanoapi.js
+++ nanoapi.js
@@ -1,6 +1,53 @@
 'use strict';
 
-var nodeShared = require('node-shared');
+var nodeShared = require('node-shared'),
+  co = require('co');
+
+function create(env, user, body) {
+  return new Promise(function (resolve, reject) {
+    env.data.apps.create(body, user, function (err, result) {
+      if (err) {
+        // Send error
+        //console.error(err);
+        return;
+      }
+      env.events.emit('app.create', user, result);
+
+      resolve(result);
+    });
+  });
+}
+
+function addKeyset(env, key, body) {
+  return new Promise(function (resolve, reject) {
+    env.data.apps.addKeyset(key, 'slack', body, function (err) {
+      console.log('addKeySet', err);
+      if (err) {
+        // Send error
+        //console.error(err);
+        reject(err);
+        return;
+      }
+
+      resolve();
+    });
+  });
+}
+
+function setBackend(env, key) {
+  return new Promise(function (resolve, reject) {
+    env.data.apps.setBackend(key, 'node', {}, function (err) {
+      if (err) {
+        // Send error
+        //console.error(err);
+        reject(err);
+        return;
+      }
+
+      resolve();
+    });
+  });
+}
 
 module.exports = function (env) {
   var auth = {};
@@ -11,46 +58,33 @@ module.exports = function (env) {
     console.log('oauth-nanoapi: hi');
     console.log('oauth-nanoapi: init', process.env.NANOAPI_URL);
     nodeShared.nano.rep.bind(process.env.NANOAPI_URL, function (nanoSocket, request, err) {
-      console.log('Received nano request:', request);
-      if (request.method === 'createSlackApp') {
-        //request.params.client_id;
-        //request.params.client_secret;
-        var user = { id: 'admin' };
-        var body = { name: request.params.guid, domains: ['https://sso.redsift.io', 'http://localhost:8081'], key: request.params.guid, secret: request.params.guid };
-        env.data.apps.create(body, user, function (err, result) {
-          if (err) {
-            // Send error
-            console.error(err);
-            return;
-          }
-          env.events.emit('app.create', user, result);
-          console.log('key=', result.key);
-          var providerBody = {
-            parameters: {
-              client_id: request.params.slack.client_id,
-              client_secret: request.params.slack.client_secret,
-              scope: 'bot identify chat:write:bot channels:read users:read'
-            }
-          };
-          env.data.apps.addKeyset(result.key, 'slack', providerBody, function (err1) {
-            console.log('addKeySet', err1);
-            if (err1) {
-              // Send error
-              console.error(err1);
-              return;
-            }
+      try {
+        co(function* () {
+          console.log('Received nano request:', request);
+          if (request.method === 'createSlackApp') {
+            var user = { id: 'admin' };
+            var body = { name: request.params.guid, domains: ['https://sso.redsift.io', 'http://localhost:8081'], key: request.params.guid, secret: request.params.guid };
+            var result = yield create(env, user, body);
 
-            // Send response result.key
-            env.data.apps.setBackend(result.key, 'node', {}, function (err2) {
-              if (err2) {
-                // Send error
-                console.error(err2);
-                return;
+            var providerBody = {
+              parameters: {
+                client_id: request.params.slack.client_id,
+                client_secret: request.params.slack.client_secret,
+                scope: 'bot identify chat:write:bot channels:read users:read'
               }
+            };
 
-            });
-          });
+            yield setBackend(env, result.key);
+
+            yield addKeyset(env, result.key, providerBody);
+
+            // Send result.key
+
+          }
         });
+      } catch (err) {
+        console.log(err);
+        // Send error
       }
     });
   };
